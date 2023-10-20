f96fa04261185232d4fff7839526eaeea02ef6d9
Afizahhh <afizahazizaf@gmail.com>
2023-10-20 13:27:55 +0800

    content main

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -12,16 +12,16 @@ const socmed = [
 export function Footer() {
   return (
     <>
-      <div className="w-fixed inset-x-0 bottom-0 bg-black py-6 ">
-        <div className="container mx-auto flex md:flex-row flex-col text-white md:px-0 px-8 space-y-8">
-            <div className="flex flex-col md:mx-auto md:w-[35%] space-y-4 airif md:text-sm text-xs">
-                <img src={logo} className="md:w-80 w-60 md:-ml-0 -ml-3" />
-                <p>
+      <div className="w-full inset-x-0 bottom-0 bg-black py-6 ">
+        <div className="container mx-auto flex md:flex-row flex-col text-white md:px-0 px-8 justify-between w-full">
+            <div className="flex flex-col md:mx-auto space-y-4 airif ">
+                <img src={logo} className="md:w-80 w-60 md:-ml-0 -ml-3 -mt-4" />
+                {/* <p>
                     Empowering Investors with Information and Access. Explore our downloadable resources and reach out to us for any inquiries or opportunities.
                 </p>
                 <p>
                     Empowering Investors with Information and Access. Explore our downloadable resources and reach out to us for any inquiries or opportunities.
-                </p>
+                </p> */}
             </div>
             <div className="flex flex-col md:mx-auto md:space-y-4 space-y-2 montserrat md:text-base text-sm">
                 <a href="#" className="font-bold">
@@ -36,8 +36,8 @@ export function Footer() {
             </div>
             <div className="flex flex-col md:mx-auto space-y-4">
                 <p className="font-bold montserrat text-base">CONTACT US</p>
-                <p className="airif md:text-sm text-xs">For business inquiries:  <br/> Hello@arkforge.gg</p>
-                <p className="airif md:text-sm text-xs">For partnership: <br/> email@partnership.com</p>
+                <p className="airif md:text-sm text-xs">For business inquiries:  <br/><a href="mailto:Hello@arkforge.gg">Hello@arkforge.gg</a></p>
+                <p className="airif md:text-sm text-xs">For partnership: <br/><a href="mailto:email@partnership.com">email@partnership.com</a></p>
                 <div className="gap-2 flex flex-row">
                     {socmed.map(socmeds => (
                         <a target="_blank" href={socmeds.link}>
