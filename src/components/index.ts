b5d904862fd23e48b73b08f5a27093ff58629d40
Alecson <alecson95@gmail.com>
2024-09-23 20:29:38 +0800

    Updates

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./form/input";
-export * from "./Icons";
-export * from "./Grid/GridComponent";
-export * from "./footer/Footer";
-export * from "./Navbar/Navbar";
--- vite.config.js
+++
@@ -1,7 +0,0 @@
-// vite.config.js
-import react from "@vitejs/plugin-react";
-import tailwindcss from "vite-plugin-tailwind";
-
-export default {
-  plugins: [react(), tailwindcss()],
-};
